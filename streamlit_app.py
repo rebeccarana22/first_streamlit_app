51433137f5cca3e5a1a03cd5b7a1c70051090f5d
rebecca rana <115029660+rebeccarana22@users.noreply.github.com>
2022-10-07 20:22:05 -0400

    Update streamlit_app.py
    badge2 lesson 9 - call the Fruityvice API from Our Streamlit App

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
