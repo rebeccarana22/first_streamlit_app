8bcfd80595e37cf0a2f76b9680ac186ed963df11
rebecca rana <115029660+rebeccarana22@users.noreply.github.com>
2022-10-09 14:45:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,4 +35,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # creates a dataframe to support in the formatting in the above line of code
 streamlit.dataframe(fruityvice_normalized)
+import snowflake.connector
 
