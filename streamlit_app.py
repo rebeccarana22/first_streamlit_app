27277374a1768e5a5d23a5f654ecad9cf7f886ce
rebecca rana <115029660+rebeccarana22@users.noreply.github.com>
2022-10-09 23:12:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ try:
  else:
   back_from_function = get_fruityvice_data(fruit_choice)
   streamlit.dataframe(back_from_function)
-except urlerror as e:
+#except urlerror as e:
  streamlit.error()
 
 
