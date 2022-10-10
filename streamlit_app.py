0c447b1a221d59fc62243737075194259f003e17
rebecca rana <115029660+rebeccarana22@users.noreply.github.com>
2022-10-09 22:12:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
@@ -24,24 +23,28 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-#new section to display fruityvice api response
+
+
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this _fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return(fruityvice_normalized) 
+   
+#new section to display fruityvice api response 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
- fruit_choice = streamlit.text_input('What fruit would you like information about?')  
-  if not fruit_choice:
-  streamlit.error ("Please select a fruit to get information.")
-else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
-  
-  except URLError as e:
-    streamlit.error()
-streamlit.write('The user entered ', fruit_choice)
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+  streamlit.error('Please select a fruit to get information.')
+ else:
+back_from_function= get_fruityvice_data(fruit_choice)
+streamlit.dataframe=(back_from_function)
+except urlerror as e:
+  streamlit.error()
 
 #dont run anything past here  while we troubleshoot     
 streamlit.stop()
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -53,4 +56,3 @@ fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfru
 streamlit.write('Thanks for adding ', fruit_choice)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
