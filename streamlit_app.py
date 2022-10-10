c86115fd26e503f1f8ee73d60f8f1ae0a6947f5e
rebecca rana <115029660+rebeccarana22@users.noreply.github.com>
2022-10-09 21:37:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) # just adds data to the screen
